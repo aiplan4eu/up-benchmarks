20129ba2dc42f6b688bff43f398bde725eb91b97
Samuel Gobbi <sgobbi@fbk.eu>
2026-04-29 10:47:17 +0200

    test: kitting generic example and fixes

--- upbm/tests/test_generic_generator.py
+++ upbm/tests/test_generic_generator.py
@@ -12,6 +12,7 @@ class TestGenericGenerator(unittest.TestCase):
     def test_generic(self):
         tests_folder = os.path.join(os.path.dirname(__file__), "test_params")
         for testfile_name in os.listdir(tests_folder):
+            print(f"\ntesting : {testfile_name}\n")
             testfile_path = os.path.join(tests_folder, testfile_name)
             test_config = {}
             with open(testfile_path) as jsonfile:
@@ -40,7 +41,7 @@ class TestGenericGenerator(unittest.TestCase):
             for instance_parameter in test_config["instance_params"].keys():
                 self.assertIn(instance_parameter, inst_space)
 
-            # instance generation - TODO
+            # instance generation
 
             instance_params = self.factory.parse_configuration(
                 test_config["instance_params"],
@@ -54,12 +55,17 @@ class TestGenericGenerator(unittest.TestCase):
 
             # Check objects
             for obj_type_name in test_config["instance_objects"].keys():
+                print(obj_type_name)
                 obj_names = [
                     o.name for o in problem.objects(problem.user_type(obj_type_name))
                 ]
+                print(obj_names)
+                print(test_config["instance_objects"][obj_type_name])
 
                 self.assertEqual(
                     len(obj_names), len(test_config["instance_objects"][obj_type_name])
                 )
-                for obj_name in obj_names:
+                for obj_name in test_config["instance_objects"][obj_type_name]:
                     self.assertIn(obj_name, obj_names)
+
+            print(f"\n{testfile_name} OK!\n")
