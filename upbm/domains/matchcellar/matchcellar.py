68c42f330d7b1698c0eaf67e89902a02905d3e62
Samuel Gobbi <sgobbi@fbk.eu>
2026-05-12 10:16:06 +0200

    chore: cleanup

--- upbm/domains/matchcellar/matchcellar.py
+++ upbm/domains/matchcellar/matchcellar.py
@@ -56,14 +56,6 @@ class MatchCellarGenerator(Generator):
 
     @property
     def instance_parameter_space(self) -> ConfigurationSpace:
-        if self.domain_params["max_matches"] == 1:
-            nmatches = 1
-        else:
-            nmatches = (1, self.domain_params["max_matches"])
-        if self.domain_params["max_fuses"] == 1:
-            nfuses = 1
-        else:
-            nfuses = (1, self.domain_params["max_fuses"])
         return ConfigurationSpace(
             {
                 "n_matches": (1)
