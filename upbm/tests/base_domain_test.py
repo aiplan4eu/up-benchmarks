a2bb30f5e483bb805d0065d93ad75e19cd7c07f5
Samuel Gobbi <sgobbi@fbk.eu>
2026-05-12 12:04:57 +0200

    chore: alternative for generic test that extends base class

--- upbm/tests/base_domain_test.py
+++ upbm/tests/base_domain_test.py
@@ -19,19 +19,37 @@ class BaseDomainTest(unittest.TestCase):
         self.object_data = {}
         self.problem_actions = {}
 
+    def get_domain_name(self) -> str:
+        return self.domain_name
+
+    def get_generator(self):
+        return self.generator
+
+    def get_validation_cases(self):
+        return self.validation_cases
+
+    def get_plannable(self):
+        return self.plannable
+
+    def get_object_data(self):
+        return self.object_data
+
+    def get_problem_actions(self):
+        return self.problem_actions
+
     def test_registration(self):
-        self.assertIn(self.domain_name, self.factory.get_registered_domains())
-        self.assertEqual(self.factory[self.domain_name], self.generator)
+        self.assertIn(self.get_domain_name(), self.factory.get_registered_domains())
+        self.assertEqual(self.factory[self.get_domain_name()], self.get_generator())
 
     def test_validation(self):
-        for (problem, plan, expected_status) in self.validation_cases:
+        for (problem, plan, expected_status) in self.get_validation_cases():
             with TimeTriggeredPlanValidator() as validator:
                 v_res = validator.validate(problem, plan)
                 self.assertEqual(v_res.status, expected_status, f"bad res:\n{v_res}")
 
     def test_planning(self):
         try:
-            for p in self.plannable:
+            for p in self.get_plannable():
                 with OneshotPlanner(problem_kind=p.kind) as planner:
                     p_res = planner.solve(p)
                     self.assertIn(
@@ -41,10 +59,10 @@ class BaseDomainTest(unittest.TestCase):
             skip("no planner available to test the problem")
 
     def test_objects_and_actions(self):
-        for problem, objects_list in self.object_data.items():
+        for problem, objects_list in self.get_object_data().items():
             for (obj_name, n_objs) in objects_list:
                 self.assertEqual(
                     sum(1 for _ in problem.objects(problem.user_type(obj_name))), n_objs
                 )
-        for problem, n_acts in self.problem_actions.items():
+        for problem, n_acts in self.get_problem_actions().items():
             self.assertEqual(len(problem.actions), n_acts)
---
+++ upbm/tests/test_matchcellar.py
@@ -0,0 +1,82 @@
+from upbm.tests.base_domain_test import BaseDomainTest
+from upbm.factory import DomainFactory
+import os
+from upbm.domains.matchcellar import MatchCellarGenerator
+from unified_planning.engines.results import ValidationResultStatus
+from unified_planning.plans import TimeTriggeredPlan
+from fractions import Fraction
+from pathlib import Path
+from ConfigSpace import Configuration
+
+
+class TestMatchcellar(BaseDomainTest):
+    __test__ = True
+
+    def get_domain_name(self):
+        return "matchcellar"
+
+    def get_generator(self):
+        return MatchCellarGenerator
+
+    def _get_instances(self):
+        gen = MatchCellarGenerator(
+            MatchCellarGenerator.get_domain_parameter_space().get_default_configuration()
+        )
+        instance_space = gen.instance_parameter_space
+        instance_1 = gen.get_instance(
+            Configuration(instance_space, {"n_matches": 3, "n_fuses": 4})
+        )
+        instance_2 = gen.get_instance(
+            Configuration(instance_space, {"n_matches": 2, "n_fuses": 2})
+        )
+        return [instance_1, instance_2]
+
+    def get_plannable(self):
+        return self._get_instances()
+
+    def get_object_data(self):
+        instances = self._get_instances()
+        object_data = {}
+        object_data[instances[0]] = [("match", 3), ("fuse", 4)]
+        object_data[instances[1]] = [("match", 2), ("fuse", 2)]
+        return object_data
+
+    def get_problem_actions(self):
+        instances = self._get_instances()
+        problem_actions = {}
+        problem_actions[instances[0]] = 2
+        problem_actions[instances[1]] = 2
+        return problem_actions
+
+    def get_validation_cases(self):
+        instances = self._get_instances()
+        light_match = instances[1].action("light_match")
+        mend_fuse = instances[1].action("mend_fuse")
+        match_1 = instances[1].object("match1")
+        fuse_0 = instances[1].object("fuse0")
+        fuse_1 = instances[1].object("fuse1")
+        valid_plan = TimeTriggeredPlan(
+            [
+                (
+                    Fraction(0, 1),
+                    light_match(match_1),
+                    Fraction(5, 1),
+                ),
+                (
+                    Fraction(1, 100),
+                    mend_fuse(fuse_0, match_1),
+                    Fraction(2, 1),
+                ),
+                (
+                    Fraction(205, 100),
+                    mend_fuse(fuse_1, match_1),
+                    Fraction(2, 1),
+                ),
+            ]
+        )
+
+        validation_cases = []
+        validation_cases.append(
+            tuple([instances[1], valid_plan, ValidationResultStatus.VALID])
+        )
+        return validation_cases
