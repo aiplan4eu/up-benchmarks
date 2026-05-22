0acc4b93ee977328fb11994d4ed7365fa71cb3d2
Samuel Gobbi <sgobbi@fbk.eu>
2026-05-22 15:25:28 +0200

    feat: long short fuses matchcellar alternative variant

--- upbm/domains/matchcellar/matchcellar.py
+++ upbm/domains/matchcellar/matchcellar.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Any, Optional, List
+from fractions import Fraction
 
 from ConfigSpace import (
     ConfigurationSpace,
@@ -8,10 +9,11 @@ from ConfigSpace import (
     Categorical,
     Constant,
 )
-from unified_planning.io import PDDLReader  # type: ignore[import-untyped]
-from unified_planning.model import Problem, Object, FNode  # type: ignore[import-untyped]
-from unified_planning.shortcuts import TRUE, UserType  # type: ignore[import-untyped]
+from unified_planning.io import PDDLReader
+from unified_planning.model import Problem, Object, FNode
+from unified_planning.shortcuts import TRUE, UserType, Real
 from typing import Any
+import math
 
 from upbm.generator import Generator
 from upbm.utils import MAX_INT, is_subspace, hyperparam_range
@@ -27,7 +29,7 @@ class MatchCellarGenerator(Generator):
         mapping: dict[str, Any] = {}
         mapping["version"] = Constant("version", 1)
         mapping["variant"] = Categorical(
-            "variant", ["ipc", "variable_duration"], default="ipc"
+            "variant", ["ipc", "variable_duration", "long_short_fuse"], default="ipc"
         )
         return ConfigurationSpace(name=mapping)
 
@@ -54,13 +56,26 @@ class MatchCellarGenerator(Generator):
     @property
     def instance_parameter_space(self) -> ConfigurationSpace:
         mapping: dict[str, Any] = {}
-        mapping["n_matches"] = Integer("n_matches", (0, MAX_INT), default=10)
-        mapping["n_fuses"] = Integer("n_fuses", (0, MAX_INT), default=16)
+        if self.variant in ["ipc", "variable_duration"]:
+            mapping["n_matches"] = Integer("n_matches", (0, MAX_INT), default=10)
+            mapping["n_fuses"] = Integer("n_fuses", (0, MAX_INT), default=16)
+        elif self.variant == "long_short_fuse":
+            mapping["short_fuses"] = Integer("short_fuses", (0, MAX_INT), default=5)
+            mapping["long_fuses"] = Integer("long_fuses", (0, MAX_INT), default=5)
+            mapping["shorts_in_one_match"] = Integer(
+                "shorts_in_one_match", (1, MAX_INT), default=2
+            )
+            mapping["longs_in_one_match"] = Integer(
+                "longs_in_one_match", (1, MAX_INT), default=1
+            )
+            mapping["extra_matches"] = Integer("extra_matches", (0, MAX_INT), default=0)
+        else:
+            raise ValueError(f"invalid variant {self.variant}")
         return ConfigurationSpace(name=mapping)
 
     @property
     def name(self) -> str:
-        return f"MatchCellar V{self.version}"
+        return f"MatchCellar V{self.version}"  # TODO add variant in name ?
 
     @property
     def domain(self) -> Problem:
@@ -77,6 +92,10 @@ class MatchCellarGenerator(Generator):
                 return reader.parse_problem(
                     str(RESOURCES_PATH / f"matchcellar_variable_duration.pddl")
                 )
+            elif self.variant == "long_short_fuse":
+                return reader.parse_problem(
+                    str(RESOURCES_PATH / f"matchcellar_long_short_fuse.pddl")
+                )
         raise ValueError(
             f"Unknown domain version {self.version} or variant {self.variant}"
         )
@@ -90,62 +109,104 @@ class MatchCellarGenerator(Generator):
 
     def get_objects(self, params) -> list[Object]:
         params.check_valid_configuration()
+        objs = []
         if not is_subspace(params.config_space, self.instance_parameter_space):
             raise ValueError(f"Invalid instance parameters: {params}")
 
-        if self.variant == "ipc" and params["n_fuses"] > 2 * params["n_matches"]:
+        if not self.check_instance_parameters(params):
             raise ValueError(f"Requested instance is unsolvable")
-
-        objs = []
-        for i in range(params["n_matches"]):
-            objs.append(self._get_object(f"match{i}", self._Match))
-        for i in range(params["n_fuses"]):
-            objs.append(self._get_object(f"fuse{i}", self._Fuse))
+        if self.variant in ["ipc", "variable_duration"]:
+            for i in range(params["n_matches"]):
+                objs.append(self._get_object(f"match{i}", self._Match))
+            for i in range(params["n_fuses"]):
+                objs.append(self._get_object(f"fuse{i}", self._Fuse))
+        elif self.variant == "long_short_fuse":
+            for i in range(params["long_fuses"]):
+                objs.append(self._get_object(f"long_fuse_{i}", self._Fuse))
+            for i in range(params["short_fuses"]):
+                objs.append(self._get_object(f"short_fuse_{i}", self._Fuse))
+            useful_matches = math.ceil(
+                params["short_fuses"] / params["shorts_in_one_match"]
+            ) + math.ceil(params["long_fuses"] / params["longs_in_one_match"])
+            for i in range(useful_matches + params["extra_matches"]):
+                objs.append(self._get_object(f"match_{i}", self._Match))
+            pass
         return objs
 
     def object_universe(
         self, instance_parameters_space: Optional[ConfigurationSpace] = None
     ):
-        if instance_parameters_space is None:
-            instance_parameters_space = self.instance_parameter_space
-        _, matches_upper = hyperparam_range(instance_parameters_space["n_matches"])
-        _, fuses_upper = hyperparam_range(instance_parameters_space["n_fuses"])
-        return [
-            self._get_object(f"match{i}", self._Match) for i in range(matches_upper)
-        ] + [self._get_object(f"fuse{i}", self._Fuse) for i in range(fuses_upper)]
+        if self.variant in ["ipc", "variable_duration"]:
+            if instance_parameters_space is None:
+                instance_parameters_space = self.instance_parameter_space
+            _, matches_upper = hyperparam_range(instance_parameters_space["n_matches"])
+            _, fuses_upper = hyperparam_range(instance_parameters_space["n_fuses"])
+            return [
+                self._get_object(f"match{i}", self._Match) for i in range(matches_upper)
+            ] + [self._get_object(f"fuse{i}", self._Fuse) for i in range(fuses_upper)]
+        elif self.variant == "long_short_fuse":
+            # TODO
+            pass
 
     def get_goal(self, params) -> list[FNode]:
         params.check_valid_configuration()
         if not is_subspace(params.config_space, self.instance_parameter_space):
             raise ValueError(f"Invalid instance parameters: {params}")
-
         res = []
-        for i in range(params["n_fuses"]):
-            res.append(self._mended(self._get_object(f"fuse{i}", self._Fuse)))
+        if self.variant in ["ipc", "variable_duration"]:
+            for i in range(params["n_fuses"]):
+                res.append(self._mended(self._get_object(f"fuse{i}", self._Fuse)))
+        elif self.variant == "long_short_fuse":
+
+            for i in range(params["long_fuses"]):
+                res.append(self._mended(self._get_object(f"long_fuse_{i}", self._Fuse)))
+            for i in range(params["short_fuses"]):
+                res.append(
+                    self._mended(self._get_object(f"short_fuse_{i}", self._Fuse))
+                )
         return res
 
     def get_initial_state(self, params) -> dict[FNode, FNode]:
         params.check_valid_configuration()
         if not is_subspace(params.config_space, self.instance_parameter_space):
             raise ValueError(f"Invalid instance parameters: {params}")
-
         res = {self._handfree(): TRUE()}
-        for i in range(params["n_matches"]):
-            res[self._unused(self._get_object(f"match{i}", self._Match))] = TRUE()
-
-        if self.variant == "variable_duration":
+        if self.variant in ["ipc", "variable_duration"]:
             for i in range(params["n_matches"]):
-                res[
-                    self._domain.fluent("match-duration")(
-                        self._get_object(f"match{i}", self._Match)
-                    )
-                ] = 5
-            for i in range(params["n_fuses"]):
+                res[self._unused(self._get_object(f"match{i}", self._Match))] = TRUE()
+
+            if self.variant == "variable_duration":
+                for i in range(params["n_matches"]):
+                    res[
+                        self._domain.fluent("match-duration")(
+                            self._get_object(f"match{i}", self._Match)
+                        )
+                    ] = 5
+                for i in range(params["n_fuses"]):
+                    res[
+                        self._domain.fluent("fuse-duration")(
+                            self._get_object(f"fuse{i}", self._Fuse)
+                        )
+                    ] = 2
+        elif self.variant == "long_short_fuse":
+            useful_matches = math.ceil(
+                params["short_fuses"] / params["shorts_in_one_match"]
+            ) + math.ceil(params["long_fuses"] / params["longs_in_one_match"])
+            for i in range(useful_matches + params["extra_matches"]):
+                res[self._unused(self._get_object(f"match_{i}", self._Match))] = TRUE()
+
+            for i in range(params["long_fuses"]):
                 res[
                     self._domain.fluent("fuse-duration")(
-                        self._get_object(f"fuse{i}", self._Fuse)
+                        self._get_object(f"long_fuse_{i}", self._Fuse)
                     )
-                ] = 2
+                ] = Real(Fraction(6, params["longs_in_one_match"]))
+            for i in range(params["short_fuses"]):
+                res[
+                    self._domain.fluent("fuse-duration")(
+                        self._get_object(f"short_fuse_{i}", self._Fuse)
+                    )
+                ] = Real(Fraction(6, params["shorts_in_one_match"]))
         return res
 
     def check_instance_parameters(self, params: Configuration):
@@ -157,4 +218,7 @@ class MatchCellarGenerator(Generator):
         ):
             # NOTE seems that for now the durations are hardcoded to be set the same way as ipc
             return False
+        if self.variant == "long_short_fuse":
+            # this specific variant is always valid
+            return True
         return True
